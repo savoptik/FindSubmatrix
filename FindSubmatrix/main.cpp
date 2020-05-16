c7257fc33b31b940fd7f94e807971a6510adba73
savoptik <sav70792@gmail.com>
2020-05-16 17:23:31 +0300

    Initial Commit

---
+++ FindSubmatrix/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FindSubmatrix
+//
+//  Created by Артём Семёнов on 16.05.2020.
+//  Copyright © 2020 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
